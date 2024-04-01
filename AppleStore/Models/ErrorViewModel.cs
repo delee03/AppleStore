9a618cc3e49dea4851ba58c207524fa14ef27d96
Pham Phat <phamphat2003vn@gmail.com>
2024-04-01 09:26:28 +0700

    Initial commit

---
+++ AppleStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppleStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
