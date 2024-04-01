344a80225c5bd574509769f8d330b03f5c9156cf
Coder1807 <bhviet.910lhphong.cr@gmail.com>
2024-04-02 00:52:39 +0700

    update ui

---
+++ AppleStore/Controllers/BlankController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppleStore.Controllers
+{
+    public class BlankController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Blank");
+        }
+    }
+}
