ba21f2520884bc5dc1b47c922cc38b952552c115
Bui Chi Bao <buichibao1601@gmail.com>
2024-04-02 20:00:02 +0700

    abc123

--- AppleStore/obj/Debug/net8.0/AppleStore.AssemblyInfo.cs
+++ AppleStore/obj/Debug/net8.0/AppleStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fb832fd6f3fd8d70fa8e2c09201102dccf1270e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b544b87d5c1b88f4982086d8223a9e8eec44d668")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
