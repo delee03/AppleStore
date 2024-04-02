f46e2d20b0d0113366b5d9b56eda55aa1b9e37da
Pham Phat <phamphat2003vn@gmail.com>
2024-04-02 10:41:51 +0700

    doc hieu

---
+++ AppleStore/Controllers/AppleStoreController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppleStore.Controllers
+{
+	public class AppleStoreController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
