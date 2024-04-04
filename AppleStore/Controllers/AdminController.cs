b2578a8d90a9058139ae73cee48baa1fbc42bde3
Coder1807 <bhviet.910lhphong.cr@gmail.com>
2024-04-04 21:49:38 +0700

    ads admin dashboard and views

---
+++ AppleStore/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppleStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("dashboard");
+        }
+
+        public IActionResult Tables()
+        {
+            return View("tables");
+        }
+
+        public IActionResult Billing()
+        {
+            return View("billing");
+        }
+
+        public IActionResult Notifications()
+        {
+            return View("notifications");
+        }
+
+        public IActionResult Profile()
+        {
+            return View("profile");
+        }
+
+        public IActionResult Signin()
+        {
+            return View("signin");
+        }
+
+        public IActionResult Signup()
+        {
+            return View("signup");
+        }
+    }
+}
--- AppleStore/obj/Debug/net8.0/AppleStore.AssemblyInfo.cs
+++ AppleStore/obj/Debug/net8.0/AppleStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+344a80225c5bd574509769f8d330b03f5c9156cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8842a37daa11400377e25746259c37fbf697626f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
