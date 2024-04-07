53c9fb5d089758110ae406f7a9fdc8f9ea470a2f
Pham Phat <phamphat2003vn@gmail.com>
2024-04-07 19:18:18 +0700

    commit quick view

--- AppleStore/obj/Debug/net8.0/AppleStore.AssemblyInfo.cs
+++ AppleStore/obj/Debug/net8.0/AppleStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d020ff30aa16244dd428f755022b831af094a909")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3204cf918c4c9f95ff31766db2ccaa6fa8c9b369")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
