f8faa51451cdb29017f933e3a0c5b98458379f2f
Coder1807 <bhviet.910lhphong.cr@gmail.com>
2024-04-07 19:33:12 +0700

    merge branch

--- AppleStore/obj/Debug/net8.0/AppleStore.AssemblyInfo.cs
+++ AppleStore/obj/Debug/net8.0/AppleStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a873d025d6a6ce2cd658c5afadff093c03ba475")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6c8afd9822c588544148f3eaac37f0ecfc20cec")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
