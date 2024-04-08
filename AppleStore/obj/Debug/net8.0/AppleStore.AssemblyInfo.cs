73c873fdf06458bb6bacd4fc802410d24015274b
Pham Phat <phamphat2003vn@gmail.com>
2024-04-08 10:06:39 +0700

    gộp

--- AppleStore/obj/Debug/net8.0/AppleStore.AssemblyInfo.cs
+++ AppleStore/obj/Debug/net8.0/AppleStore.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53c9fb5d089758110ae406f7a9fdc8f9ea470a2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abc4fc9ef883c83dd160eb7ae23108f3b465a8fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
