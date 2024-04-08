a3ee59e302a3d22c4f1c183ed14914a8c8cd5203
Bui Chi Bao <buichibao1601@gmail.com>
2024-04-08 11:25:30 +0700

    test

---
+++ AppleStore/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppleStore.Models
+{
+	public class ApplicationUser : IdentityUser
+	{
+
+		[Required]
+		public string FullName { get; set; }
+		public string? Address { get; set; }
+		public string? Age { get; set; }
+	}
+}
