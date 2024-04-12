c7887a4e31ce9de822145f3a0ce714b30ed5661e
Pham Phat <phamphat2003vn@gmail.com>
2024-04-12 15:12:27 +0700

    sửa form checkout

--- AppleStore/obj/Debug/net8.0/AppleStore.AssemblyInfo.cs
+++ AppleStore/obj/Debug/net8.0/AppleStore.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec53ab2f0a7df85480109a4ee5b41473a7b34f6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+338a06157b4e6de7a58bbd01937f36738b3a12d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
